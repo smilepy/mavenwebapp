8a3b699c3e061e7e8fa15406a3337a21e32e2fff
smilepy <peiyuclx@163.com>
2016-12-12 18:11:23 +0800

    add dockerfile

--- src/main/java/com/autohome/practice/web/controller/IndexController.java
+++ src/main/java/com/autohome/practice/web/controller/IndexController.java
@@ -5,14 +5,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.apache.log4j.Logger;
+
+import java.util.Date;
 
 /**
  * Created by smilepy on 2016/11/15.
  */
 @Controller
 public class IndexController {
+    public static Logger printLog = Logger.getLogger("print-log");
     @RequestMapping(value = "/")
     public ModelAndView getUser(){
+        printLog.info("this is IndexController   "+new Date());
         ModelAndView mv = new ModelAndView("/vm2/index");
         String environment= TestClass.Environment;
         mv.addObject("environment",environment);
